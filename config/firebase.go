de194a86cfb80a127f6a58f8dc234f3f944a7100
zaqimaulana <zaqimaulana72@gmail.com>
2026-06-29 23:18:16 +0700

    inisialisasi Firebase Messaging client

--- config/firebase.go
+++ config/firebase.go
@@ -7,26 +7,30 @@ import (
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
+	"firebase.google.com/go/v4/messaging"
 	"google.golang.org/api/option"
 )
 
-// FirebaseAuth adalah instance Firebase Auth yang dipakai untuk verify token
 var FirebaseAuth *auth.Client
+var FirebaseMessaging *messaging.Client
 
 func InitFirebase() {
 	credPath := os.Getenv("FIREBASE_CREDENTIALS_PATH")
 
-	// Inisialisasi Firebase App dengan service account credentials
 	opt := option.WithCredentialsFile(credPath)
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("Gagal init Firebase: %v", err)
 	}
 
-	// Dapatkan Firebase Auth client
 	FirebaseAuth, err = app.Auth(context.Background())
 	if err != nil {
 		log.Fatalf("Gagal mendapatkan Firebase Auth client: %v", err)
+	}
+
+	FirebaseMessaging, err = app.Messaging(context.Background())
+	if err != nil {
+		log.Fatalf("Gagal mendapatkan Firebase Messaging client: %v", err)
 	}
 
 	log.Println("Firebase Admin SDK berhasil diinisialisasi")
